09264c5c3960f752d9b29eacb1d2ed7eedfd7faa
Kelvin Wambua <62960869+kelvinwambua@users.noreply.github.com>
2024-02-21 13:49:15 +0300

    Delete process.js

--- process.js
+++
@@ -1,7 +0,0 @@
-const knowledgeAndBeliefs = require('./read');
-
-knowledgeAndBeliefs.forEach(function(data, index){
-    let {Age, Category, Percentage, Gender} = data;
-    console.log(data)
-
-})
